a14ad01fcb55d59bd6b0735a4ca8c6046726db23
david <dspiller321@gmail.com>
2026-05-21 16:35:23 +0300

    add function get_solider_duties

--- duty_manager.py
+++ duty_manager.py
@@ -4,3 +4,6 @@ def add_duty_to_solider(solider_id:int,duty_name:str,day:str):#מוסיף תור
 def update_duty_status(solider_id:int,duty_name:str,new_status:str):#מעדכנת סטטוס תורנות
     pass
 
+def get_solider_duties(solider_id:int):#מחזיר רשימת תורנויות של חייל
+    pass
+
