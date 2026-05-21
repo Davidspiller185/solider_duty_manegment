4fb4f1a75676a24f3d07859327d0900358bcdcd8
david <dspiller321@gmail.com>
2026-05-21 16:51:36 +0300

    add solider_has_duty function

--- utils.py
+++ utils.py
@@ -7,3 +7,6 @@ def is_valid_status(status:str) ->bool:# בדיקה אם הסטטוס חוקי
 def is_valid_name(name:str) ->bool:#בדיקה אם השם תקין שהוא לא ריק
     pass
 
+def solider_has_duty(solider:dict,duty_name:str) ->bool:#בדיקה אם לחייל יש כבר את אותה תורנות עם השם המסויים
+    pass
+
