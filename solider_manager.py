5f0fb5c0f1b723dc010acb7a1f47c097bebd50af
david <dspiller321@gmail.com>
2026-05-21 16:10:37 +0300

    add function add_solider

---
+++ solider_manager.py
@@ -0,0 +1,10 @@
+def add_solider(solider_id:int,name:str):#מוסיפה חייל חדש למערכת
+    pass
+
+def remove_solider(solider_id:int):#מסירה חייל מהמערכת לפי id
+    pass
+
+def get_all_solider():# מחזירה את רשימת כל החיילים במערכת
+    pass
+
+
