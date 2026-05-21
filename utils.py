e20478a9cb9e348fea072514180af774654c70e8
david <dspiller321@gmail.com>
2026-05-21 16:48:57 +0300

    add is_valid_name function

--- utils.py
+++ utils.py
@@ -4,3 +4,6 @@ def find_solider_by_id(solider_id:int) ->dict |None:#מחפשת חייל לפי 
 def is_valid_status(status:str) ->bool:# בדיקה אם הסטטוס חוקי
     pass
 
+def is_valid_name(name:str) ->bool:#בדיקה אם השם תקין שהוא לא ריק
+    pass
+
