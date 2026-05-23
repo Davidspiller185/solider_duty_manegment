81cc33fb6896370fe9b279734e57f533e34a5435
david <dspiller321@gmail.com>
2026-05-24 00:53:38 +0300

    add data function

---
+++ data.py
@@ -0,0 +1,15 @@
+def dic_solder():
+    dic_solider={"id":int,
+                 "name":str,
+                 "duties":list}
+    return dic_solider
+
+def dic_shifts():
+    dic_shift={"name":str,
+               "day":str,
+               "status":str}
+    return dic_shift
+
+def lst_solder():
+    lst_solider=[]
+    return lst_solider
