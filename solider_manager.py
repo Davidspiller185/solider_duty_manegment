88f609f5acd648fffd843ff79dbd1309f620a3f5
david <dspiller321@gmail.com>
2026-05-26 00:41:59 +0300

    add the body of the function

--- solider_manager.py
+++ solider_manager.py
@@ -1,11 +1,25 @@
-def add_solider(solider_id:int,name:str):#מוסיפה חייל חדש למערכת
-    pass
+from data import dic_solder,lst_solder
+from utils import find_solider_by_id
+def add_solider(solider_id:int,name:str)->None:#מוסיפה חייל חדש למערכת
+    dic_solider=dic_solder()
+    dic_solider["id"]=solider_id
+    dic_solider["name"]=name
+    lst=lst_solder()
+    lst.append(dic_solider)
+    
 
-def remove_solider(solider_id:int):#מסירה חייל מהמערכת לפי id
-    pass
+def remove_solider(solider_id:int)->None:#מסירה חייל מהמערכת לפי id
+    lst=lst_solder()
+    result=find_solider_by_id(solider_id)
+    if result is not None:
+        lst.remove(result)
+    else:
+        raise KeyError("the solider id not defined")
+    
 
-def get_all_solider():# מחזירה את רשימת כל החיילים במערכת
-    pass
+def get_all_solider()->list:# מחזירה את רשימת כל החיילים במערכת
+    lst=lst_solder()
+    return lst
 
 
 
