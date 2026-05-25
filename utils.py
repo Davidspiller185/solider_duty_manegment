a91f9848e57fe62c604ac472f7b35983a9cf148d
david <dspiller321@gmail.com>
2026-05-26 00:43:08 +0300

    add the body of the function

--- utils.py
+++ utils.py
@@ -1,15 +1,33 @@
+from data import lst_solder
 def find_solider_by_id(solider_id:int) ->dict |None:#מחפשת חייל לפי id ומחזירה אותו
-    pass
+    lst=lst_solder()
+    for dic in lst:
+        if dic["id"]==solider_id:
+            return dic
+    return None
 
 def is_valid_status(status:str) ->bool:# בדיקה אם הסטטוס חוקי
-    pass
+    lst_status=["pending","completed","missed"]
+    if status not in lst_status:
+        return False
+    return True
 
 def is_valid_name(name:str) ->bool:#בדיקה אם השם תקין שהוא לא ריק
-    pass
+    if name:
+        return True
+    return False
 
 def solider_has_duty(solider:dict,duty_name:str) ->bool:#בדיקה אם לחייל יש כבר את אותה תורנות עם השם המסויים
-    pass
+    for dic in solider["duties"]:
+        if dic["name"]==duty_name:
+            return True
+    return False
 
 def is_valid_day(dat:str) ->bool:#בדיקה אם היום הוא חוקי שהוא לא יום שישי או שבת 
-    pass
+    lst_days=["sunday","monday","tuesday","wednesday","thursday"]
+    lst_not_valid=["friday","saturday"]
+    if dat not in lst_days or dat in lst_not_valid:
+        return False
+    return True
+    
 
